5fcb4ae65ad49682e78365fdd8f7dec778df4ab1
Nicholas Huber <anongenie@gmail.com>
2022-08-20 03:54:22 -0600

    Update tabs.js

--- src/modules/my/app/tabs.js
+++ src/modules/my/app/tabs.js
@@ -3,17 +3,7 @@ const tabItems = {
         title: 'Components',
         value: 'components',
         visible: false
-    },
-    /*lockersafejs: {
-        title: 'Locker Safe JS Resources',
-        value: 'lockersafejs',
-        visible: false
-    },
-    learning: {
-        title: 'Learning LWC + JS',
-        value: 'learning',
-        visible: false
-    }*/
+    }
 };
 
 const tabElements = ['components', 'lockersafejs', 'learning'];
